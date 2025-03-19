f79830ae3014bea4cd71e6e2c5b016b0e7a641f0
Aminat Adebayo <adebayo.aminat@gmail.com>
2025-03-19 14:06:10 -0400

    updates

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
-import ImageSection from '../Components/ImageSection'
 import Contact from '../Components/Contact'
 import SocialMedias from '../Components/SocialMedias'
 import Description from '../Components/Description'
