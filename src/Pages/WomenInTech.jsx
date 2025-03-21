e1579056030f8c2a31995f13dfc16e5f0f5bc127
logan-lampton <lampton.logan@gmail.com>
2025-03-21 13:19:25 -0400

    rough formatting for all components

--- src/Pages/WomenInTech.jsx
+++ src/Pages/WomenInTech.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ImageSection from '../Components/ImageSection'
 import Description from '../Components/Description'
 
+
 const WomenInTech = () => {
   return (
     <div>
